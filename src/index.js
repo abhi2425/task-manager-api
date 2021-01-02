f9c638b1949af49b31961088ea6521e28bbf142c
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-01-02 17:25:27 +0530

    removing unncessary comments

--- src/index.js
+++ src/index.js
@@ -14,22 +14,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up at ", port);
 });
-// const Users = require("./models/users");
-// const Tasks = require("./models/tasks");
-// const main = async () => {
-//   const task = await Tasks.findById("5fef79d67f682f273ce14481");
-//   await task.populate("owner").execPopulate();
-//   console.log(task);
-
-//   //   Tasks.findById("5fef79d67f682f273ce14481").then((task) => {
-//   //     task
-//   //       .populate("owner")
-//   //       .execPopulate()
-//   //       .then((data) => console.log(data));
-//   //   });
-
-//   const user = await Users.findById("5fef27afbe60b7255cc56644");
-//   await user.populate("tasks").execPopulate();
-//   console.log(user);
-// };
-// main();
