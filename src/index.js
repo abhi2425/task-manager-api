310f33f7de2f194bd93fc62df5a76978d6775a43
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-01-03 05:28:04 +0530

    response header set for cross origin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("./db/mongoose");
-require("dotenv").config()
+require("dotenv").config();
 
 const app = express();
 const userRouter = require("./routers/userRouter");
@@ -8,6 +8,10 @@ const taskRouter = require("./routers/taskRouter");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(userRouter);
 app.use(taskRouter);
 
