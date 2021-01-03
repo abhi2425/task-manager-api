e02a8a9f227d7896380cc06a715bbedbc81431a3
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-01-03 13:25:25 +0530

    cors fixed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 require("./db/mongoose");
 require("dotenv").config();
-
+const cors = require("cors");
 const app = express();
 const userRouter = require("./routers/userRouter");
 const taskRouter = require("./routers/taskRouter");
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use(userRouter);
 app.use(taskRouter);
-
 app.listen(port, () => {
   console.log("Server is up at ", port);
 });
