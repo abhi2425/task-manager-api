9005ef01f2c4cf900c4622dd512f5ef8bfaf8342
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-01-24 00:36:18 +0530

    fixed user delete prblm

--- src/models/users.js
+++ src/models/users.js
@@ -1,116 +1,125 @@
-require("dotenv").config()
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const Tasks = require("./tasks");
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        uppercase: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-        validate(value) {
+require('dotenv').config()
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Tasks = require('./tasks')
+const userSchema = new mongoose.Schema(
+   {
+      name: {
+         type: String,
+         required: true,
+         trim: true,
+         uppercase: true,
+      },
+      email: {
+         type: String,
+         required: true,
+         trim: true,
+         unique: true,
+         validate(value) {
             if (!validator.isEmail(value)) {
-                throw new Error("Not Valid Email!!");
+               throw new Error('Not Valid Email!!')
             }
-        },
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
+         },
+      },
+      age: {
+         type: Number,
+         default: 0,
+         validate(value) {
             if (value < 0) {
-                throw new Error("Age can not be negative");
+               throw new Error('Age can not be negative')
             }
-        },
-    },
-    password: {
-        required: true,
-        type: String,
-        trim: true,
-        validate(value) {
+         },
+      },
+      password: {
+         required: true,
+         type: String,
+         trim: true,
+         validate(value) {
             if (value.length < 8) {
-                throw new Error("Password Must be greater Than 7 Characters");
-            } else if (value.toLowerCase().includes("password")) {
-                throw new Error("Password can't set be password");
+               throw new Error('Password Must be greater Than 7 Characters')
+            } else if (value.toLowerCase().includes('password')) {
+               throw new Error("Password can't set be password")
             }
-        },
-    },
-    tokens: [{
-        token: {
-            type: String,
-            required: true,
-        },
-    }, ],
-    avatar: {
-        type: Buffer,
-    },
-}, {
-    timestamps: true,
-});
-userSchema.virtual("tasks", {
-    ref: "Tasks",
-    localField: "_id",
-    foreignField: "owner",
-});
+         },
+      },
+      tokens: [
+         {
+            token: {
+               type: String,
+               required: true,
+            },
+         },
+      ],
+      avatar: {
+         type: Buffer,
+      },
+   },
+   {
+      timestamps: true,
+   },
+)
+userSchema.virtual('tasks', {
+   ref: 'Tasks',
+   localField: '_id',
+   foreignField: 'owner',
+})
 userSchema.methods.toJSON = function () {
-    const userObject = this.toObject();
-    delete userObject.password;
-    delete userObject.tokens;
-    delete userObject.avatar
-    return userObject;
-};
+   const userObject = this.toObject()
+   delete userObject.password
+   delete userObject.tokens
+   delete userObject.avatar
+   return userObject
+}
 
 userSchema.methods.getAuthToken = async function () {
-    const token = jwt.sign({
-        _id: this._id.toString()
-    }, process.env.SECRET_KEY, {
-        expiresIn: "2 days",
-    });
-    this.tokens = this.tokens.concat({
-        token
-    });
-    await this.save();
-    return token;
-};
+   const token = jwt.sign(
+      {
+         _id: this._id.toString(),
+      },
+      process.env.SECRET_KEY,
+      {
+         expiresIn: '2 days',
+      },
+   )
+   this.tokens = this.tokens.concat({
+      token,
+   })
+   await this.save()
+   return token
+}
 
 userSchema.statics.findByCredential = async (email, password) => {
-    const user = await User.findOne({
-        email: email
-    });
+   const user = await User.findOne({
+      email: email,
+   })
 
-    if (!user) {
-        throw new Error("E-Mail Not Found!!!");
-    }
-    const isMatch = bcryptjs.compare(password, user.password);
-    if (!isMatch) {
-        throw new Error("Unable To Login");
-    }
-    return user;
-};
+   if (!user) {
+      throw new Error('E-Mail Not Found!!!')
+   }
+   const isMatch = bcryptjs.compare(password, user.password)
+   if (!isMatch) {
+      throw new Error('Unable To Login')
+   }
+   return user
+}
 
 //Hashing The Password Before Saving
-userSchema.pre("save", async function (next) {
-    const user = this;
+userSchema.pre('save', async function (next) {
+   const user = this
 
-    if (user.isModified("password")) {
-        user.password = await bcryptjs.hash(user.password, 8);
-    }
-    next();
-});
-userSchema.pre("remove", async function (next) {
-    Tasks.deleteMany({
-        owner: this._id
-    });
-    next();
-});
+   if (user.isModified('password')) {
+      user.password = await bcryptjs.hash(user.password, 8)
+   }
+   next()
+})
+userSchema.pre('remove', async function (next) {
+   await Tasks.deleteMany({
+      owner: this._id,
+   })
+   next()
+})
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+const User = mongoose.model('User', userSchema)
+module.exports = User
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,183 +1,173 @@
-const express = require("express");
-const Users = require("../models/users");
-const auth = require("../middleware/auth");
-const multer = require("multer");
-const sharp = require("sharp");
-const User = require("../models/users");
-const sendEmail = require("../account/account");
-const router = new express.Router();
-module.exports = router;
+const express = require('express')
+const Users = require('../models/users')
+const auth = require('../middleware/auth')
+const multer = require('multer')
+const sharp = require('sharp')
+const User = require('../models/users')
+const sendEmail = require('../account/account')
+const router = new express.Router()
+module.exports = router
 
 //signing User
-router.post("/users", async (req, res) => {
-  const user = new Users(req.body);
-  const message = `Welcome ${user.name} to Task Manger Api. Let me know your Experience with api`;
-  try {
-    await user.save();
-    sendEmail(user.email, message);
-    const token = await user.getAuthToken();
-    res.status(201).send({
-      user,
-      token,
-    }); //automatically calls JSON.Stringify()
-  } catch (error) {
-    res.status(400).send("Error!! " + error);
-  }
-});
+router.post('/users', async (req, res) => {
+   const user = new Users(req.body)
+   const message = `Welcome ${user.name} to Task Manger Api. Let me know your Experience with api`
+   try {
+      await user.save()
+      sendEmail(user.email, message)
+      const token = await user.getAuthToken()
+      res.status(201).send({
+         user,
+         token,
+      }) //automatically calls JSON.Stringify()
+   } catch (error) {
+      res.status(400).send('Error!! ' + error)
+   }
+})
 //logging User
-router.post("/users/login", async (req, res) => {
-  try {
-    const user = await Users.findByCredential(
-      req.body.email,
-      req.body.password
-    );
-    const token = await user.getAuthToken();
+router.post('/users/login', async (req, res) => {
+   try {
+      const user = await Users.findByCredential(req.body.email, req.body.password)
+      const token = await user.getAuthToken()
 
-    res.send({
-      user,
-      token,
-    });
-  } catch (error) {
-    res.status(404).send({
-      error: "User Not Found",
-    });
-  }
-});
+      res.send({
+         user,
+         token,
+      })
+   } catch (error) {
+      res.status(404).send({
+         error: 'User Not Found',
+      })
+   }
+})
 //logout User
-router.post("/users/logout", auth, (req, res) => {
-  try {
-    req.user.tokens = req.user.tokens.filter(
-      (token) => token.token !== req.token
-    );
-    req.user.save();
-    res.status(200).send({
-      message: "Logged Out!!",
-    });
-  } catch (error) {
-    res.status(500).send({
-      error: error.message,
-    });
-  }
-});
+router.post('/users/logout', auth, (req, res) => {
+   try {
+      req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
+      req.user.save()
+      res.status(200).send({
+         message: 'Logged Out!!',
+      })
+   } catch (error) {
+      res.status(500).send({
+         error: error.message,
+      })
+   }
+})
 
 //logout User From All Places
-router.post("/users/logoutAll", auth, (req, res) => {
-  try {
-    req.user.tokens = [];
-    req.user.save();
-    res.status(201).send({
-      message: "Logged Out !! From All",
-    });
-  } catch (error) {
-    res.status(500).send({
-      error: error.message,
-    });
-  }
-});
+router.post('/users/logoutAll', auth, (req, res) => {
+   try {
+      req.user.tokens = []
+      req.user.save()
+      res.status(201).send({
+         message: 'Logged Out !! From All',
+      })
+   } catch (error) {
+      res.status(500).send({
+         error: error.message,
+      })
+   }
+})
 
 // User Profile After Logging in
-router.get("/users/me", auth, (req, res) => {
-  res.send(req.user);
-});
+router.get('/users/me', auth, (req, res) => {
+   res.send(req.user)
+})
 
-router.patch("/users/me", auth, async ({ body, user }, res) => {
-  const userUpdates = Object.keys(body);
-  const allowedUpdates = ["name", "age", "email", "password"];
-  const isValidUpdate = userUpdates.every((updates) =>
-    allowedUpdates.includes(updates)
-  );
-  if (!isValidUpdate) {
-    const invalidProperty = [];
-    userUpdates.forEach((key) => {
-      if (!allowedUpdates.includes(key)) {
-        invalidProperty.push(key);
+router.patch('/users/me', auth, async ({ body, user }, res) => {
+   const userUpdates = Object.keys(body)
+   const allowedUpdates = ['name', 'age', 'email', 'password']
+   const isValidUpdate = userUpdates.every((updates) => allowedUpdates.includes(updates))
+   if (!isValidUpdate) {
+      const invalidProperty = []
+      userUpdates.forEach((key) => {
+         if (!allowedUpdates.includes(key)) {
+            invalidProperty.push(key)
+         }
+      })
+      return res.status(404).send(`Error:- Invalid Property-${invalidProperty} is Not Updated `)
+   }
+   try {
+      userUpdates.forEach((update) => {
+         user[update] = body[update]
+      })
+      await user.save()
+      if (!user) {
+         return res.status(404).send()
       }
-    });
-    return res
-      .status(404)
-      .send(`Error:- Invalid Property-${invalidProperty} is Not Updated `);
-  }
-  try {
-    userUpdates.forEach((update) => {
-      user[update] = body[update];
-    });
-    await user.save();
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.status(200).send(user);
-  } catch (error) {
-    res.status(500).send({
-      error: "error!!" + error.message,
-    });
-  }
-});
+      res.status(200).send(user)
+   } catch (error) {
+      res.status(500).send({
+         error: 'error!!' + error.message,
+      })
+   }
+})
 
-router.delete("/users/me", auth, async (req, res) => {
-  try {
-    const message =
-      "Thanks for Using my app. Please let us Know what we are lacking ";
-    const user = await req.user.remove();
-    sendEmail(user.email, message);
-    res.send(user);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
+router.delete('/users/me', auth, async (req, res) => {
+   try {
+      const message = 'Thanks for Using my app. Please let us Know what we are lacking '
+      const user = await req.user.remove()
+      sendEmail(user.email, message)
+      res.send(user)
+   } catch (error) {
+      res.status(500).send()
+   }
+})
 const upload = multer({
-  limits: {
-    fileSize: 2000000,
-  },
-  // fileFilter(req, file, cb) {
-  //   if (!file.originalname.match(/\.(png|jpg|jpeg|svg)$/)) {
-  //     return cb(new Error("Please Upload image file only"))
-  //   }
-  //   cb(undefined, true)
-  // },
-});
+   limits: {
+      fileSize: 2000000,
+   },
+   // fileFilter(req, file, cb) {
+   //   if (!file.originalname.match(/\.(png|jpg|jpeg|svg)$/)) {
+   //     return cb(new Error("Please Upload image file only"))
+   //   }
+   //   cb(undefined, true)
+   // },
+})
 
 router.post(
-  "/users/me/avatar",
-  auth,
-  upload.single("avatar"),
-  async (req, res) => {
-    const buffer = await sharp(req.file.buffer)
-      .png()
-      .resize({
-        width: 400,
-        height: 400,
+   '/users/me/avatar',
+   auth,
+   upload.single('avatar'),
+   async (req, res) => {
+      const buffer = await sharp(req.file.buffer)
+         .png()
+         .resize({
+            width: 400,
+            height: 400,
+         })
+         .toBuffer()
+      req.user.avatar = buffer
+      await req.user.save()
+      res.status(200).send({
+         message: 'FileUploaded',
       })
-      .toBuffer();
-    req.user.avatar = buffer;
-    await req.user.save();
-    res.status(200).send({
-      message: "FileUploaded",
-    });
-  },
-  (error, _, res, next) => {
-    res.status(400).send({
-      error: error.message,
-    });
-  }
-);
-router.get("/users/:id/avatar", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user || !user.avatar) {
-      throw new Error("Image Not Found!");
-    }
-    res.set("Content-Type", "image/png");
-    res.status(200).send(user.avatar);
-  } catch (error) {
-    res.send({
-      error: error.message,
-    });
-  }
-});
-router.delete("/users/me/avatar", auth, async (req, res) => {
-  req.user.avatar = undefined;
-  await req.user.save();
-  res.status(200).send({
-    message: "Image Deleted",
-  });
-});
+   },
+   (error, _, res, next) => {
+      res.status(400).send({
+         error: error.message,
+      })
+   },
+)
+router.get('/users/:id/avatar', async (req, res) => {
+   try {
+      const user = await User.findById(req.params.id)
+      if (!user || !user.avatar) {
+         throw new Error('Image Not Found!')
+      }
+      res.set('Content-Type', 'image/png')
+      res.status(200).send(user.avatar)
+   } catch (error) {
+      res.send({
+         error: error.message,
+      })
+   }
+})
+router.delete('/users/me/avatar', auth, async (req, res) => {
+   req.user.avatar = undefined
+   await req.user.save()
+   res.status(200).send({
+      message: 'Image Deleted',
+   })
+})
