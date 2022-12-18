91195f6edc9964ac993db455f5c619e135e2a521
Abhinav Jha <abhi.mount.26@gmail.com>
2022-12-18 19:01:44 +0530

    "/" route handled for showing link to postman collection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const app = require('./app')
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+   return res.send({
+      hello: 'Welcome to task-manager api',
+      message: 'Please use postman for interacting with the api.',
+      postmanCollection: "https://github.com/abhi2425/task-manager-api/blob/master/Task%20Manager.postman_collection.json"
+   });
+})
+
 app.listen(port, () => {
    console.log('Server is up at ', port)
 })
